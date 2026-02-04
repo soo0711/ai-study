0811d6934febdae002192b7c77472857329304a8
soo0711 <k300hys@naver.com>
2026-02-04 18:31:07 +0900

    chore: init study project

---
+++ src/main/java/com/ai_study/AiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ai_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai_study/AiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
